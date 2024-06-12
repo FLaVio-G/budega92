f045dff4061c328d3210be421d8929f82f0c9f47
KaTSu113 <flaviogabrielandrade@gmail.com>
2024-06-12 17:53:02 -0300

    Fix: Color changes and responsiveness adjustments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
 import Link from "next/link";
+import type { MenuProps } from "antd";
+import { Button, Dropdown } from "antd";
 
 interface NavLinkProps {
   href: string;
@@ -15,18 +17,72 @@ const NavLink: React.FC<NavLinkProps> = ({ href, children }) => (
   </Link>
 );
 
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        Bebidas
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        Mapa
+      </a>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        Sobre
+      </a>
+    ),
+  },
+  {
+    key: "4",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        Em breve
+      </a>
+    ),
+  },
+];
+
 export default function Header() {
   return (
     <header className="w-full h-28 flex px-8 font-mono text-sm items-center justify-between">
       <div className="flex">
         <NavLink href="/">Budega 92</NavLink>
       </div>
-      <div className="hidden   lg:flex space-x-8">
+      <div className="hidden lg:flex space-x-8">
         <NavLink href="/bebidas">Bebidas</NavLink>
         <NavLink href="/mapa">Mapa</NavLink>
         <NavLink href="/sobre">Sobre</NavLink>
         <NavLink href="/">Em breve</NavLink>
       </div>
+      <Dropdown className="lg:hidden" menu={{ items }} placement="bottomLeft">
+        <Button>Menu</Button>
+      </Dropdown>
     </header>
   );
 }
--- src/pages/bebidas.tsx
+++ src/pages/bebidas.tsx
@@ -1,76 +1,74 @@
 "use client";
-import React from "react";
-import Image from "next/image";
 
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import cerveja1 from "../../public/images/cervejas.jpg";
-import garrafa1 from "../../public/images/garrafa-agua.jpg";
-import Jambu1 from "../../public/images/jambu.jpg";
-import snake1 from "../../public/images/snakes.jpg";
-import vinho1 from "../../public/images/vinho.jpg";
+import { CardProduct } from "@/components/CardProduct";
+import { gql, useQuery } from "@apollo/client";
+import Image from "next/image";
 
-import "keen-slider/keen-slider.min.css";
+const GET_ALL_PRODUCT = gql`
+  query AllProducts {
+    products {
+      description
+      id
+      name
+      price
+      typeDrink
+      image {
+        url
+      }
+    }
+  }
+`;
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  typeDrink: string;
+  image: {
+    url: string;
+  }[];
+}
+
+interface AllProductsData {
+  products: Product[];
+}
 
 export default function Bebidas() {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const { data, loading, error } = useQuery<AllProductsData>(GET_ALL_PRODUCT);
+
+  const handleClick = () => {
+    setShowOverlay(true);
+  };
+
+  const handleClose = () => {
+    setShowOverlay(false);
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data?.products);
+
   return (
     <div className="flex mx-auto max-w-7xl flex-col items-center justify-between">
       <Header />
 
-      <div className="grid grid-cols-2 bg-gray-100 p-8 rounded-lg  md:grid-cols-3  gap-8">
-        <a href="/destilados">
-          <div>
-            <div className="h-[350px] w-[350px] rounded-lg text-white">
-              <Image src={Jambu1} alt="Picture of the author" />
-            </div>
-            <p className="flex font-semibold text-gray-800  justify-center">
-              Destilados
-            </p>
-          </div>
-        </a>
-
-        <a className="rounded-lg" href="/destilados">
-          <div>
-            <div className="h-[350px] w-[350px] text-white">
-              <Image src={vinho1} alt="Picture of the author" />
-            </div>
-            <p className="flex font-semibold text-gray-800  justify-center">
-              Vinho
-            </p>
-          </div>
-        </a>
-
-        <a href="/destilados">
-          <div>
-            <div className="h-[350px] w-[350px] text-white">
-              <Image src={cerveja1} alt="Picture of the author" />
-            </div>
-            <p className="flex font-semibold text-gray-800  justify-center">
-              Cervejas
-            </p>
-          </div>
-        </a>
-        <a href="/destilados">
-          <div>
-            <div className="h-[350px] w-[350px] text-white">
-              <Image src={snake1} alt="Picture of the author" />
-            </div>
-            <p className="flex font-semibold text-gray-800  justify-center">
-              Diversos
-            </p>
-          </div>
-        </a>
-
-        <a href="/destilados">
-          <div>
-            <div className="h-[350px] w-[350px] text-white">
-              <Image src={garrafa1} alt="Picture of the author" />
-            </div>
-            <p className="flex font-semibold text-gray-800  justify-center">
-              Água
-            </p>
-          </div>
-        </a>
+      <div className="grid grid-cols-4 gap-8 bg-gray-100 text-gray-800 rounded-lg p-8 items-center justify-center ">
+        {data?.products.map((product) => (
+          <CardProduct
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            description={product.description}
+            typeDrink={product.typeDrink}
+            urlImage={product.image[0]?.url}
+          />
+        ))}
       </div>
 
       <Footer />
